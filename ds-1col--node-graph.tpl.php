ccef51e7aff332eb0598b0ad135d4517b8e4c442
Margery Tongway <pstampy@gmail.com>
2013-11-29 10:29:08 +1100

    Update ds-1col--node-graph.tpl.php
    Now with extra bounce (pie chart bounces)

--- ds-1col--node-graph.tpl.php
+++ ds-1col--node-graph.tpl.php
@@ -151,31 +151,22 @@ window.onload = function () {
   var pie = r.g.piechart(320, 240, 100, values, { legend: legend, legendpos: "east"});
 
   r.text(320, 100, "I'm a rad pie chart").attr({ font: "20px sans-serif" });
-  
-   pie.hover(function () {
- 
-
-
- this.sector.stop();
-   this.sector.scale(1.1, 1.1, this.cx, this.cy);
-
-   if (this.label) {
-     this.label[0].stop();
-     this.label[0].attr({ r: 7.5 });
-     this.label[1].attr({ "font-weight": 800 });
-   }
- },
-
- function () {
-   this.sector.scale(1.0, 1.0, this.cx, this.cy);
-
-   if (this.label) {
-     this.label[0].animate({ r: 5 }, 500, "bounce");
-     this.label[1].attr({ "font-weight": 400 });
-   }
- });
-};
-
+    pie.hover(function () {
+                    this.sector.stop();
+                    this.sector.scale(1.1, 1.1, this.cx, this.cy);
+                    if (this.label) {
+                        this.label[0].stop();
+                        this.label[0].scale(1.5);
+                        this.label[1].attr({"font-weight": 800});
+                    }
+                }, function () {
+                    this.sector.animate({scale: [1, 1, this.cx, this.cy]}, 500, "bounce");
+                    if (this.label) {
+                        this.label[0].animate({scale: 1}, 500, "bounce");
+                        this.label[1].attr({"font-weight": 400});
+                    }
+                });
+    };
 
 
 </script>
